74e0d1e83785a1842a207b411653560189f3c831
marcosMartinezSoftwareoneSantiago <Marcos.Martinez@Softwareone.com>
2023-01-18 13:51:38 +0100

    Proyecto de nuevo

---
+++ ejercicio1reactnet/backNet/Models/Cliente.cs
@@ -0,0 +1,16 @@
+﻿namespace backNet.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string email { get; set; }
+
+        public Cliente(int Id, string Nombre, string email)
+        {
+            this.Id = Id;
+            this.Nombre = Nombre;
+            this.email = email;
+        }
+    }
+}
