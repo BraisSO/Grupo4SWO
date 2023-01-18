fbce4604221edac6d6f9c256081e151b14944974
Brais <103991765+BraisSO@users.noreply.github.com>
2023-01-18 16:48:14 +0100

    post repair *spread operator*

--- ejercicio1reactnet/frontReact/src/Components/GuardarComponent/Guardar.jsx
+++ ejercicio1reactnet/frontReact/src/Components/GuardarComponent/Guardar.jsx
@@ -2,8 +2,6 @@ import axios from 'axios';
 import { useState } from 'react';
 import "./Guardar.css"
 
-//REPARAR ESTO, SOLO DETECTA O ULTIMO VALOR ENVIADO
-
 export default function Guardar() {
 const [clientes, setClientes] = useState ({
 nombre: '',
@@ -28,14 +26,15 @@ function handleSubmit(e){
                 name="nombre" 
                 placeholder='nombre'
                 value={clientes.nombre}
-                onChange={e => setClientes({ nombre: e.target.value })} 
+                onChange={e => setClientes({ ...clientes,nombre: e.target.value })} 
             ></input>
             <input 
                 type="text" 
                 name="correo"
                 placeholder='correo'
                 value={clientes.correo}
-                onChange={e => setClientes({ correo: e.target.value })}>
+                onChange={e => setClientes({ ...clientes,correo: e.target.value })}>  {/*Hai que usar el spread operator, porque sino el objeto se 
+                sobreescribe entero pasando uno de los campos a ser undefined*/}
             </input>
                 <button type="submit">Submit</button>
             </form>
