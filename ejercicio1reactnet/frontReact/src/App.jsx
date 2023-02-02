d7ff704919487465c434ff235dc0cabeda200fe8
Brais <103991765+BraisSO@users.noreply.github.com>
2023-02-02 11:52:14 +0100

    cambios estéticos

--- ejercicio1reactnet/frontReact/src/App.jsx
+++ ejercicio1reactnet/frontReact/src/App.jsx
@@ -5,10 +5,14 @@ import List from './Components/ListComponent/List'
 
 function App() {
   return (
-   <>
+  <div className='globalContainer'>
+    <div className='section'>
     <List/>
+    </div>
+    <div className='section'>
     <Guardar/>
-   </>
+    </div>
+  </div>
   )
 }
 
